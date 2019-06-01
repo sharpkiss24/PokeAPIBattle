91d2f356417b3b03ac15cdfc5415affbde0f3ed8
sharpkiss24 <shariana.allen@gmail.com>
2019-06-01 12:19:48 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 $(document).ready(function(){
-
-
 //starter function to pull a random pokemon from the PokeAPI
   function getRandomPokemonLeft() {
     let randomNumber = Math.floor(Math.random() * 800) + 1;
@@ -10,7 +8,7 @@ $(document).ready(function(){
   }
 
 
- function getRandomPokemonLeft() {
+ function getRandomPokemonRight() {
     let randomNumber = Math.floor(Math.random() * 800) + 1;
     $.get(`https://pokeapi.co/api/v2/pokemon/${randomNumber}`).then(function(pokeData){
       console.log("Right Pokemon Data: ", pokeData);
