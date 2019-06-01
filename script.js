089550b99f7198dca1d7d87875fa9ef679022c76
sharpkiss24 <shariana.allen@gmail.com>
2019-06-01 12:02:38 -0400

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,17 @@ $(document).ready(function(){
   }
 
 
+ function getRandomPokemonLeft() {
+    let randomNumber = Math.floor(Math.random() * 800) + 1;
+    $.get(`https://pokeapi.co/api/v2/pokemon/${randomNumber}`).then(function(pokeData){
+      console.log("Right Pokemon Data: ", pokeData);
+    });
+  }
 
+
+
+$("#leftButton").click(getRandomPokemonLeft);
+$("#rightButton").click(getRandomPokemonRight);
 //all code here
 
 
